eeefbba806f973c175be4539fe34d29b6d2cc303
Md. Safiul Alam Rafi <sarafi3258@gmail.com>
2025-07-01 13:17:22 +0600

    update

---
+++ class/1-7.php
@@ -0,0 +1,23 @@
+<?php
+class table{
+    public static $name = "naman";
+
+    public static function show() {
+        echo "This is static method";
+        echo self::$name;
+    }
+}
+echo table::$name;
+table::show();
+class tool extends table{
+    public $age = 25;
+    
+    public function shine() {
+        return parent::$name;
+        return $age;
+    }
+}
+
+$nam = new tool();
+echo $nam->shine();
+?>
